01a365bac10c838cb89dc05e6d6d76c116ae7a0a
Mayank Agarwal <mayank.agarwal1@spglobal.com>
2026-05-13 11:51:19 +0530

    Added starting services code

--- Services/app/src/main/java/com/example/services/MainActivity.kt
+++ Services/app/src/main/java/com/example/services/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.app.Service
+import android.content.Intent
+import android.os.Build
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        
+
+        // way to start the Service and can pass your own service as a Intent class perimeter
+        val intent = Intent(this, Service::class.java)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            startForegroundService(intent)
+        }else{
+            startService(intent)
+        }
     }
 }
